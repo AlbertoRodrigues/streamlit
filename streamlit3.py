2b49d42a41b107390bd1c45a2fa7b8dd9f15bf70
Alberto Rodrigues <albertor@ime.usp.br>
2023-11-19 16:16:44 -0300

    first commit

---
+++ streamlit3.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from streamlit_chat import message
+
+st.set_page_config(
+    page_title="Streamlit Chat - Demo",
+    page_icon=":robot:"
+)
+
+
+st.header("Streamlit Chat - Demo")
+st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+#message("My message") 
+#message("Hello bot!", is_user=True) 
+
+
+user_input = get_text()
+
+if user_input:
+ 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append("Olá!")
+    
+    
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
